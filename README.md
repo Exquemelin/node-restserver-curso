85799900f53f7bdb2557a22d2ba43851ebf78e2a
Ivan <ivan.chao.ballesteros@gmail.com>
2020-11-17 18:01:23 +0100

    Primer Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Aplicación de restserver
+
+Este es el ejercicio del curso
+
+Recuerden instalar los paquetes de node
+```
+npm install
+```
---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+// ==============================
+// PUERTO
+// ==============================
+process.env.PORT = process.env.PORT || 3000 // Establecemos que el puerto sea el del process y si no, que tome uno por defecto
