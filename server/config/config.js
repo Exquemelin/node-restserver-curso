21c7b36c8a4693eba5d92ce20e819f5afe06efc3
Ivan <ivan.chao.ballesteros@gmail.com>
2020-11-18 14:16:38 +0100

    Nuevo Entorno

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://exquemelin:ETw4FLPmtZ2QkSEL@clustercursonode.ksxtc.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
